b4a4a63f046410d72fda3aff4c1bf3f4282b659e
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2022-06-11 08:30:34 -0700

    udpate:

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-// Ex-01: Write a function in JS that takes a string and revers
+// Ex-01: Write a function in JS that takes a string and reverse 
+
+function reversestr(str){
+    var listofchar=str.split('');
+    var reverse=listofchar.reverse();
+    var str=reverse.join('');
+
+    return str;
+
+}
+console.log(reversestr("hello world"))
+
+
+// ex-02 Write a javascript function that check for palindrome or not
+function ispalindrome(str){
+
+    var stre=reversestr(str);
+
+    if(stre===reversestr){
+        return true;
+
+    }
+    else{
+        return false;
+
+    }
+}
+
+console.log(ispalindrome("civic"))
+
+
 
 
 
