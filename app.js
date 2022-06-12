889489bac1ffdcec0796bfc5fa7b85f5f2a8fe40
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2022-06-12 13:46:44 -0700

    fixing error:

--- app.js
+++ app.js
@@ -57,10 +57,10 @@ function convertdatetostring(date){
     return datestr;
 
 }
-
+// Input: { day: 5, month: 1, year: 2020 }
 var date={
-    day:10,
-    month:9,
+    day:5,
+    month:1,
     year:2020
 }
 console.log(convertdatetostring(date));
@@ -76,15 +76,10 @@ function checkallvariation(date){
     var datestr=convertdatetostring(date);
 
     var ddmmyyyy=datestr.day+datestr.month+datestr.year;
-
     var mmddyyyy=datestr.month+datestr.day+datestr.year;
-
     var yyyymmdd=datestr.year+datestr.month+datestr.day;
-
     var ddmmyy=datestr.day+datestr.month+datestr.year.slice(-2);
-
     var mmddyy=datestr.month+datestr.day+datestr.year.slice(-2);
-
     var yymmdd=datestr.year.slice(-2)+datestr.month+datestr.day;
 
     return [ddmmyyyy,mmddyyyy,yyyymmdd,ddmmyy,mmddyy,yymmdd]
@@ -126,8 +121,94 @@ function isleapyear(year){
 
     }
     return false;
-    
+
 }
+// console.log(isleapyear(2021))
+function getnextdate(date){
+    // in this function only we will increment the day here month and year remains same;
+    var day=date.day+1;
+
+    var month=date.month;
+    var year=date.year;
+
+    // for daysinmonth
+    var daysinmonth=[31,28,31,30,31,30,31,31,30,31,30,31];
+
+    if(month==2){
+        // checking for leapyear;
+        if(isleapyear(year)){
+            if(day>29){
+                day=1;
+                month++;
+
+            }
+            else{
+                if(day>28){
+                    day=1;
+                    month++;
+
+                }
+            }
+        }
+    }
+    else{
+        // except leapyear
+        // if lastdays exceeds in the month
+        if(day>daysinmonth[month -1]){
+            day=1;
+            month++;
+
+        }
+    }
+    // suppose if month exceeds 
+    if(month>12){
+        month=1;
+        year++;
+
+    }
+    return {
+        day:day,
+        month:month,
+        year:year
+    }
+}
+var date2={
+    day:29,
+    month:02,
+    year:2020
+}
+console.log(getnextdate(date2));
+
+
+
+function nextpalindromedate(date){
+    var ctr=0;
+    var nextdate=getnextdate(date);
+
+    while(1){
+        ctr++;
+        // now we will use ex-04 function to checkthepalindrome
+        var ispalindrome=checkallvariation(nextdate);
+
+        if(ispalindrome){
+            break;
+        }
+        // now will search again to get the valid palindrome
+        nextdate=getnextdate(nextdate);
+    }
+    return [ctr,nextdate];
+
+
+}
+var date10={
+    day:01,
+    month:04,
+    year:2020
+}
+console.log(nextpalindromedate(date10))
+
+// console.log(nextpalindromedate(date))
+
 
 
 
