fd8eaba6a349ce8c2a1470a37614f18411f1b103
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2022-06-12 11:42:00 -0700

    update:ex-03 function convertdatetostring is completed

--- app.js
+++ app.js
@@ -25,6 +25,45 @@ function ispalindrome(str){
 }
 console.log(ispalindrome("civic"))
 
+// Ex-03: Write a function that converts the date from number to string
+
+function convertdatetostring(date){
+    var datestr={
+        day:'',
+        month:'',
+        year:''
+
+    }
+
+    if(date.day<10){
+        datestr.day='0'+date.day;
+
+    }
+    else{
+        datestr.day=date.day.toString();
+    }
+    // similiar for the month
+    if(date.month<10){
+        datestr.month='0'+date.month;
+    }
+    else{
+        datestr.month=date.month.toString();
+    }
+
+    // for year it will remain same
+    datestr.year=date.year.toString();
+
+    // now we have to return the datestr
+    return datestr;
+    
+}
+
+var date={
+    day:4,
+    month:5,
+    year:2020
+}
+console.log(convertdatetostring(date));
 
 
 
